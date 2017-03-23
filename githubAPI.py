07596c8f57b7acce09982a7126ecbfecda6afbca
asmsuechan <suenagaryoutaabc@gmail.com>
2017-03-23 12:24:29 -0700

    Add mongodb and connect it to python container

--- githubAPI.py
+++ githubAPI.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import pdb
 from pprint import pprint
+import langid
+import base64
+import pymongo
 
 load_dotenv(find_dotenv())
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
@@ -10,21 +13,23 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
 g = Github(login_or_token=GITHUB_ACCESS_TOKEN, per_page=100)
 
 # Get repositories which have over 5000 stars and count it
-# repositories = g.search_repositories('stars:>5000')
-# number_of_repos = 0
-# for page_number in range(9):
-#   for repo in repositories.get_page(page_number):
-#     number_of_repos += 1
-#     print(repo)
-# print(number_of_repos)
+repositories = g.search_repositories('stars:>5000')
+number_of_repos = 0
+repos = []
+for page_number in range(9):
+  for repo in repositories.get_page(page_number):
+    number_of_repos += 1
+    # flatten clasified list and repository information
+    repos.append([langid.classify(base64.b64decode(repo.get_readme().content)), repo])
 
+pdb.set_trace()
 # Get stargazers of the repository
-repository = g.get_repo('BoostIO/Boostnote')
-stargazers = repository.get_stargazers()
-for page_number in range(100):
-  for stargazer in stargazers.get_page(page_number):
-      pdb.set_trace()
-      print(stargazer)
+# repository = g.get_repo('BoostIO/Boostnote')
+# stargazers = repository.get_stargazers()
+# for page_number in range(100):
+#   for stargazer in stargazers.get_page(page_number):
+#       pdb.set_trace()
+#       print(stargazer)
 
 # ~~~ MY PYTHON TRAINING ~~~
 # pdb.set_trace()
